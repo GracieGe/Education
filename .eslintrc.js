77be82e489953d379ae7084494a4ace8ee03c122
GracieGe <ruiyi.ge2001@gmail.com>
2024-06-28 18:07:19 +0100

    .eslintrc.js update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,30 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native-community', 
+    'prettier', 
+  ],
+  plugins: ['react', 'react-native', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    'react-native/react-native': true,
+  },
+  rules: {
+    // Custom role
+    'prettier/prettier': ['error', {
+      arrowParens: 'avoid',
+      bracketSameLine: true,
+      bracketSpacing: false,
+      singleQuote: true,
+      trailingComma: 'all',
+    }],
+  },
 };
