9675e9c9b9b72dbcc3aaf1fd11456325e0bb49b0
GracieGe <ruiyi.ge2001@gmail.com>
2024-07-17 19:45:04 +0100

    test2

--- screens/Home.js
+++ screens/Home.js
@@ -54,7 +54,7 @@ const Home = ({ navigation }) => {
       <View style={styles.headerContainer}>
         <View style={styles.viewLeft}>
           <Image
-            source={images.user2}
+            source={images.user1}
             resizeMode='contain'
             style={styles.userIcon}
           />
