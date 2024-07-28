3841e1c8e5980423d9ad142ecb4d79e5d61ea683
GracieGe <ruiyi.ge2001@gmail.com>
2024-07-28 22:18:06 +0100

    change the page structure of AddNewAddress

--- screens/AddNewAddress.js
+++ screens/AddNewAddress.js
@@ -1,240 +1,83 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity, StatusBar } from 'react-native';
-import React, { useRef, useEffect, useReducer, useCallback, useState } from 'react';
-import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import { View, Text, StyleSheet, TouchableOpacity, StatusBar } from 'react-native';
+import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { icons, SIZES, COLORS, FONTS } from '../constants';
-import RBSheet from 'react-native-raw-bottom-sheet';
+import { SIZES, COLORS } from '../constants';
 import { commonStyles } from '../styles/CommonStyles';
 import Input from '../components/Input';
-import { validateInput } from '../utils/actions/formActions';
-import { reducer } from '../utils/reducers/formReducers';
 import Button from '../components/Button';
-import { mapStandardStyle } from '../data/mapData';
-
-const initialState = {
-  inputValues: {
-    address: '',
-    street: '',
-    postalCode: '',
-    appartment: '',
-  },
-  inputValidities: {
-    address: false,
-    street: false,
-    postalCode: false,
-    appartment: false,
-  },
-  formIsValid: false,
-}
+import Header from '../components/Header';
 
 const AddNewAddress = ({ navigation }) => {
-  const bottomSheetRef = useRef(null)
-  const [error, setError] = useState()
-  const [formState, dispatchFormState] = useReducer(reducer, initialState)
+  const [address1, setAddress1] = useState('');
+  const [city, setCity] = useState('');
+  const [postalCode, setPostalCode] = useState('');
+  const [address2, setAddress2] = useState('');
   const [selectedLabel, setSelectedLabel] = useState(null);
 
   const handleLabelSelection = (label) => {
-    setSelectedLabel(label)
-  }
+    setSelectedLabel(label);
+  };
 
-  const inputChangedHandler = useCallback(
-    (inputId, inputValue) => {
-      const result = validateInput(inputId, inputValue)
-      dispatchFormState({ inputId, validationResult: result, inputValue })
-    },
-    [dispatchFormState]
-  )
-
-  useEffect(() => {
-    if (error) {
-      Alert.alert('An error occured', error)
-    }
-  }, [error])
-
-  // Open the bottom sheet on component mount
-  useEffect(() => {
-    bottomSheetRef.current.open()
-  }, [])
+  const saveLocationHandler = () => {
+    navigation.navigate('Address');
+  };
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
-      <StatusBar hidden={true} />
-      <View
-        style={{
-          position: 'absolute',
-          marginHorizontal: 16,
-          position: 'absolute',
-          flexDirection: 'row',
-          alignItems: 'center',
-          top: 22,
-          zIndex: 999,
-        }}
-      >
-        <TouchableOpacity
-          onPress={() => navigation.goBack()}
-          style={{
-            height: 45,
-            width: 45,
-            borderRadius: 22.5,
-            backgroundColor: COLORS.black,
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginRight: 16,
-            zIndex: 9999,
-          }}
-        >
-          <Image
-            source={icons.arrowLeft}
-            resizeMode="contain"
-            style={{
-              height: 24,
-              width: 24,
-              tintColor: COLORS.white,
-            }}
-          />
-        </TouchableOpacity>
-        <Text style={{ ...FONTS.body3 }}>Add New Address</Text>
-      </View>
-      <MapView
-        style={styles.map}
-        provider={PROVIDER_GOOGLE}
-        customMapStyle={mapStandardStyle}
-        initialRegion={{
-          latitude: 48.8566,
-          longitude: 2.3522,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        <Marker
-          coordinate={{
-            latitude: 48.8566,
-            longitude: 2.3522,
-          }}
-          image={icons.mapMarkerIcon}
-          title="Move"
-          description="Address"
-          onPress={() => console.log('Move to another screen')}
-        >
-          <Callout tooltip>
-            <View>
-              <View style={styles.bubble}>
-                <Text
-                  style={{
-                    ...FONTS.body4,
-                    fontWeight: 'bold',
-                    color: COLORS.black,
-                  }}
-                >
-                  User Address
-                </Text>
-              </View>
-              <View style={styles.arrowBorder} />
-              <View style={styles.arrow} />
-            </View>
-          </Callout>
-        </Marker>
-      </MapView>
-      <RBSheet
-        ref={bottomSheetRef}
-        height={500}
-        openDuration={250}
-        closeOnDragDown={true}
-        closeOnPressMask={false}
-        customStyles={{
-          wrapper: {
-            backgroundColor: 'transparent',
-          },
-          draggableIcon: {
-            backgroundColor: COLORS.gray2,
-            width: 100,
-          },
-          container: {
-            backgroundColor: COLORS.white,
-            paddingVertical: 16
-          }
-        }}
-      >
-        <View
-          style={{
-            width: SIZES.width - 32,
-            marginHorizontal: 16,
-          }}
-        >
+    <SafeAreaView style={styles.area}>
+      <StatusBar hidden={false} />
+      <View style={styles.container}>
+        <Header title="Add New Address" />
+        <View style={styles.contentContainer}>
           <View style={{ flexDirection: 'row' }}>
             <View style={{ marginVertical: 0 }}>
-              <View
-                style={{
-                  marginTop: 0,
-                  width: SIZES.width - 32,
-                }}
-              >
-                <Text style={[commonStyles.inputHeader, {
-                  color: COLORS.greyscale900
-                }]}>
-                  Address
+              <View style={styles.inputContainer}>
+                <Text style={[commonStyles.inputHeader, { color: COLORS.greyscale900 }]}>
+                  Address 1
                 </Text>
                 <Input
                   id="address"
-                  onInputChanged={inputChangedHandler}
-                  errorText={formState.inputValidities['address']}
-                  placeholder="3235 Royal Ln. mesa, new jersy 34567"
+                  value={address1}
+                  onChangeText={setAddress1}
+                  placeholder=""
                   placeholderTextColor={COLORS.black}
                 />
               </View>
 
-              <View style={{ marginTop: 12 }}>
-                <Text style={[commonStyles.inputHeader, {
-                  color: COLORS.greyscale900
-                }]}>
-                  Appartment
+              <View style={styles.inputContainer}>
+                <Text style={[commonStyles.inputHeader, { color: COLORS.greyscale900 }]}>
+                  Address 2
                 </Text>
                 <Input
                   id="appartment"
-                  onInputChanged={inputChangedHandler}
-                  errorText={formState.inputValidities['appartment']}
-                  placeholder="2143"
+                  value={address2}
+                  onChangeText={setAddress2}
+                  placeholder=""
                   placeholderTextColor={COLORS.black}
                 />
               </View>
 
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                  marginTop: 12,
-                }}>
-                <View
-                  style={{
-                    width: (SIZES.width - 32) / 2 - 10,
-                  }}>
-                  <Text style={[commonStyles.inputHeader, {
-                    color: COLORS.greyscale900
-                  }]}>
-                    Street
+              <View style={styles.rowContainer}>
+                <View style={styles.halfWidthInput}>
+                  <Text style={[commonStyles.inputHeader, { color: COLORS.greyscale900 }]}>
+                    City
                   </Text>
                   <Input
                     id="street"
-                    onInputChanged={inputChangedHandler}
-                    errorText={formState.inputValidities['street']}
-                    placeholder="hason nagar"
+                    value={city}
+                    onChangeText={setCity}
+                    placeholder=""
                     placeholderTextColor={COLORS.black}
                   />
                 </View>
-                <View
-                  style={{
-                    width: (SIZES.width - 32) / 2 - 10,
-                  }}>
-                  <Text style={[commonStyles.inputHeader, {
-                    color: COLORS.greyscale900
-                  }]}>
+                <View style={styles.halfWidthInput}>
+                  <Text style={[commonStyles.inputHeader, { color: COLORS.greyscale900 }]}>
                     Post Code
                   </Text>
                   <Input
                     id="postalCode"
-                    onInputChanged={inputChangedHandler}
-                    errorText={formState.inputValidities['postalCode']}
-                    placeholder="3456"
+                    value={postalCode}
+                    onChangeText={setPostalCode}
+                    placeholder=""
                     placeholderTextColor={COLORS.black}
                   />
                 </View>
@@ -242,36 +85,22 @@ const AddNewAddress = ({ navigation }) => {
             </View>
           </View>
           <View>
-            <Text
-              style={{
-                fontSize: 13,
-                fontFamily: "Urbanist Regular",
-                marginBottom: 2,
-                color: COLORS.greyscale900
-              }}>
-              AVAILABLE TIME
+            <Text style={[commonStyles.inputHeader, { color: COLORS.greyscale900 }]}>
+              Label
             </Text>
 
-            <View
-              style={{ flexDirection: 'row', marginVertical: 13 }}
-            >
+            <View style={{ flexDirection: 'row', marginVertical: 13 }}>
               <TouchableOpacity
                 style={[
                   styles.checkboxContainer,
-                  selectedLabel === 'home' &&
-                  styles.selectedCheckbox,
-                  {
-                    borderColor: COLORS.greyscale900
-                  }
+                  selectedLabel === 'home' && styles.selectedCheckbox,
+                  { borderColor: COLORS.greyscale900 }
                 ]}
                 onPress={() => handleLabelSelection('home')}>
                 <Text
                   style={[
-                    selectedLabel === 'home' &&
-                    styles.checkboxText,
-                    {
-                      color: selectedLabel === 'home' ? COLORS.white : COLORS.primary 
-                    }
+                    selectedLabel === 'home' && styles.checkboxText,
+                    { color: selectedLabel === 'home' ? COLORS.white : COLORS.primary }
                   ]}>
                   Home
                 </Text>
@@ -280,19 +109,14 @@ const AddNewAddress = ({ navigation }) => {
               <TouchableOpacity
                 style={[
                   styles.checkboxContainer,
-                  selectedLabel === 'work' &&
-                  styles.selectedCheckbox,
-                  {
-                    borderColor: COLORS.greyscale900
-                  }
+                  selectedLabel === 'work' && styles.selectedCheckbox,
+                  { borderColor: COLORS.greyscale900 }
                 ]}
                 onPress={() => handleLabelSelection('work')}>
                 <Text
                   style={[
-                    selectedLabel === 'work' &&
-                    styles.checkboxText, {
-                      color: selectedLabel === 'work' ? COLORS.white : COLORS.primary 
-                    }
+                    selectedLabel === 'work' && styles.checkboxText,
+                    { color: selectedLabel === 'work' ? COLORS.white : COLORS.primary }
                   ]}
                 >
                   Work
@@ -302,21 +126,15 @@ const AddNewAddress = ({ navigation }) => {
               <TouchableOpacity
                 style={[
                   styles.checkboxContainer,
-                  selectedLabel === 'other' &&
-                  styles.selectedCheckbox,
-                  {
-                    borderColor: COLORS.greyscale900
-                  }
+                  selectedLabel === 'other' && styles.selectedCheckbox,
+                  { borderColor: COLORS.greyscale900 }
                 ]}
                 onPress={() => handleLabelSelection('other')}
               >
                 <Text
                   style={[
-                    selectedLabel === 'other' &&
-                    styles.checkboxText,
-                    {
-                      color: selectedLabel === 'other' ? COLORS.white : COLORS.greyscale900
-                    }
+                    selectedLabel === 'other' && styles.checkboxText,
+                    { color: selectedLabel === 'other' ? COLORS.white : COLORS.greyscale900 }
                   ]}
                 >
                   Other
@@ -326,82 +144,41 @@ const AddNewAddress = ({ navigation }) => {
             <Button
               filled
               title="SAVE LOCATION"
-              onPress={() => {
-                bottomSheetRef.current.close()
-                navigation.navigate('Address')
-              }}
-              style={{
-                borderRadius: 30
-              }}
+              onPress={saveLocationHandler}
+              style={{ borderRadius: 30 }}
             />
           </View>
         </View>
-      </RBSheet>
+      </View>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  map: {
-    height: '100%',
-    zIndex: 1,
+  area: {
+    flex: 1,
+    backgroundColor: COLORS.white,
   },
-  // Callout bubble
-  bubble: {
-    flexDirection: 'column',
-    alignSelf: 'flex-start',
-    backgroundColor: '#fff',
-    borderRadius: 6,
-    borderColor: '#ccc',
-    borderWidth: 0.5,
-    padding: 15,
-    width: 'auto',
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+    padding: 16,
   },
-  // Arrow below the bubble
-  arrow: {
-    backgroundColor: 'transparent',
-    borderColor: 'transparent',
-    borderTopColor: '#fff',
-    borderWidth: 16,
-    alignSelf: 'center',
-    marginTop: -32,
+  contentContainer: {
+    paddingHorizontal: 16,
+    paddingVertical: 16,
   },
-  arrowBorder: {
-    backgroundColor: 'transparent',
-    borderColor: 'transparent',
-    borderTopColor: '#007a87',
-    borderWidth: 16,
-    alignSelf: 'center',
-    marginTop: -0.5,
+  inputContainer: {
+    marginTop: 12,
+    width: SIZES.width - 32,
   },
-  body3: {
-    fontSize: 12,
-    color: COLORS.gray5,
-    marginVertical: 3,
+  rowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 12,
   },
-  h3: {
-    fontSize: 12,
-    color: COLORS.gray5,
-    marginVertical: 3,
-    fontFamily: "Urbanist Bold",
-    marginRight: 6,
-  },
-  btn1: {
-    width: 45,
-    height: 45,
-    borderRadius: 22.5,
-    backgroundColor: COLORS.primary,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  btn2: {
-    width: 45,
-    height: 45,
-    borderRadius: 22.5,
-    borderColor: COLORS.primary,
-    borderWidth: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  halfWidthInput: {
+    width: (SIZES.width - 32) / 2 - 10,
   },
   checkboxContainer: {
     paddingHorizontal: 8,
@@ -414,17 +191,6 @@ const styles = StyleSheet.create({
     marginRight: 12,
     marginBottom: 12,
   },
-  roundedCheckBoxContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: 48,
-    width: 48,
-    borderRadius: 22,
-    borderWidth: 1,
-    borderColor: COLORS.gray,
-    backgroundColor: COLORS.gray,
-    marginRight: 12,
-  },
   selectedCheckbox: {
     backgroundColor: COLORS.primary,
   },
@@ -433,16 +199,6 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontFamily: "Urbanist Regular",
   },
-  starContainer: {
-    height: 48,
-    width: 48,
-    borderRadius: 24,
-    borderWidth: 1,
-    borderColor: COLORS.secondaryGray,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginRight: 6,
-  },
 })
 
-export default AddNewAddress
+export default AddNewAddress;
